1600042dfbe987a68862b253ec24be9a57078d37
anton-novak <anton-novak@outlook.com>
2023-05-12 12:13:47 +0100

    time periods fix

--- client/utils/thisTimePeriod.ts
+++ client/utils/thisTimePeriod.ts
@@ -16,6 +16,18 @@ export default function getThisPeriod(periodString) {
     end.setMonth(end.getMonth() + 1);
     end.setDate(1);
     return [setTimeTo00andISO(start), setTimeTo00andISO(end)];
+  } else if (periodString === "this_quarter") {
+    let start = new Date();
+    while ((start.getMonth() + 1) % 3 !== 0) {
+      start.setMonth(start.getMonth() - 1);
+    };
+    start.setDate(1);
+    let end = new Date();
+    do {
+      start.setMonth(start.getMonth() + 1);
+    } while ((start.getMonth() + 1) % 3 !== 0);
+    end.setDate(1);
+    return [setTimeTo00andISO(start), setTimeTo00andISO(end)];
   } else if (periodString === "this_year") {
     let start = new Date();
     start.setMonth(0);
