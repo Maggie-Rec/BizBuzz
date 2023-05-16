f77bc2848edf67654d6a6e9715fc476933e3bec8
Maggie Recio <m.reciogrom@gmail.com>
2023-05-16 11:54:55 +0100

    form field reset and send user to db

--- client/components/ProfileInfoTab.tsx
+++ client/components/ProfileInfoTab.tsx
@@ -2,7 +2,7 @@
 
 import styles from "../styles/ProfileInfoTab.module.css";
 import React, { useState } from "react";
-import { Collapse, Input, Button } from "antd";
+import { Collapse, Input, Button, Form } from "antd";
 const { Panel } = Collapse;
 import { postUserDetails } from "../components/ApiService/userDetails";
 
@@ -18,7 +18,8 @@ const ProfileInfoTab = () => {
     setter(event);
   }
 
-  function handleSubmit() {
+  const [form] = Form.useForm();
+  const handleSubmit = ({ name, last, addres1, addres2, area, city1, type1 }) => {
     const user = {
       firstName: firstName,
       lastName: lastName,
@@ -27,9 +28,11 @@ const ProfileInfoTab = () => {
       city: city,
       type: type,
     };
+    console.log(user);
 
-    postUserDetails(user);
-  }
+    // postUserDetails(user);
+    form.resetFields();
+  };
   function changeAdress1(newAddress: string) {
     let temp = address;
     temp[0] = newAddress;
@@ -41,6 +44,8 @@ const ProfileInfoTab = () => {
     temp[1] = newAddress;
     setAddress([...temp]);
   }
+
+  // const onFinish = (values) => {};
 
   return (
     <div className={styles.container}>
@@ -61,67 +66,89 @@ const ProfileInfoTab = () => {
           <p>{type}</p>
         </Panel>
         <Panel header="Your Locations" key={3} style={{ fontWeight: "bold" }}>
-          <p>- Barcelona</p>
-          <p>- London</p>
-          <p>- Berlin</p>
+          <p> {city}</p>
         </Panel>
         <Panel header="Update your details" key={4}>
-          <Input
-            type="text"
-            onChange={(event: any) =>
-              handleChange(event.target.value, setFirstName)
-            }
-            placeholder="First Name"
-            className={styles.businessInput}
-          />
-          <Input
-            type="text"
-            onChange={(event: any) =>
-              handleChange(event.target.value, setLastName)
-            }
-            placeholder="Last Name"
-            className={styles.businessInput}
-          />
-          <h3>Business Address</h3>
-          <Input
-            type="text"
-            onChange={(event: any) =>
-              handleChange(event.target.value, changeAdress1)
-            }
-            placeholder="Number and Street name"
-            className={styles.businessInput}
-          />
-          <Input
-            type="text"
-            onChange={(event: any) =>
-              handleChange(event.target.value, changeAdress2)
-            }
-            placeholder="Post code"
-            className={styles.businessInput}
-          />
-          <Input
-            type="text"
-            onChange={(event: any) =>
-              handleChange(event.target.value, setRegion)
-            }
-            placeholder="Region"
-            className={styles.businessInput}
-          />
-          <Input
-            type="text"
-            onChange={(event: any) => handleChange(event.target.value, setCity)}
-            placeholder="City"
-            className={styles.businessInput}
-          />
-          <Input
-            type="text"
-            onChange={(event: any) => handleChange(event.target.value, setType)}
-            placeholder="Type: (Onsite / Online)"
-            className={styles.businessInput}
-          />
-          <Button type="primary" onClick={() => handleSubmit()}>
-            Submit
-          </Button>
+          <Form onFinish={handleSubmit} form={form}>
+            <Form.Item name="name">
+              <Input
+                type="text"
+                onChange={(event: any) =>
+                  handleChange(event.target.value, setFirstName)
+                }
+                placeholder="First Name"
+                className={styles.businessInput}
+              />
+            </Form.Item>
+            <Form.Item name="last">
+              <Input
+                type="text"
+                onChange={(event: any) =>
+                  handleChange(event.target.value, setLastName)
+                }
+                placeholder="Last Name"
+                className={styles.businessInput}
+              />
+            </Form.Item>
+            <h3>Business Address</h3>
+            <Form.Item name="address1">
+              <Input
+                type="text"
+                onChange={(event: any) =>
+                  handleChange(event.target.value, changeAdress1)
+                }
+                placeholder="Number and Street name"
+                className={styles.businessInput}
+              />
+            </Form.Item>
+            <Form.Item name="address2">
+              <Input
+                type="text"
+                onChange={(event: any) =>
+                  handleChange(event.target.value, changeAdress2)
+                }
+                placeholder="Post code"
+                className={styles.businessInput}
+              />
+            </Form.Item>
+            <Form.Item name="area">
+              <Input
+                type="text"
+                onChange={(event: any) =>
+                  handleChange(event.target.value, setRegion)
+                }
+                placeholder="Region"
+                className={styles.businessInput}
+              />
+            </Form.Item>
+            <Form.Item name="city1">
+              <Input
+                type="text"
+                onChange={(event: any) =>
+                  handleChange(event.target.value, setCity)
+                }
+                placeholder="City"
+                className={styles.businessInput}
+              />
+            </Form.Item>
+            <Form.Item name="type1">
+              <Input
+                type="text"
+                onChange={(event: any) =>
+                  handleChange(event.target.value, setType)
+                }
+                placeholder="Type: (Onsite / Online)"
+                className={styles.businessInput}
+              />
+            </Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              
+            >
+              Submit
+            </Button>
+          </Form>
         </Panel>
       </Collapse>
     </div>
