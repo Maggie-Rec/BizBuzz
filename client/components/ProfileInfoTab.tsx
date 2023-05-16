b53c6fd9433283a226b58381d003cbd86a0332dc
anton-novak <anton-novak@outlook.com>
2023-05-16 15:01:52 +0100

    chore: clean up merge markers

--- client/components/ProfileInfoTab.tsx
+++ client/components/ProfileInfoTab.tsx
@@ -7,15 +7,6 @@ const { Panel } = Collapse;
 import { postUserDetails } from "../components/ApiService/userDetails";
 
 const ProfileInfoTab = () => {
-<<<<<<< HEAD
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [address, setAddress] = useState(['', '']);
-
-  function handleChange() {
-    // ...
-  }
-=======
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [address, setAddress] = useState(["", ""]);
@@ -38,7 +29,6 @@ const ProfileInfoTab = () => {
       type: type,
     };
     console.log(user);
->>>>>>> refactor-new
 
     // postUserDetails(user);
     form.resetFields();
@@ -58,69 +48,6 @@ const ProfileInfoTab = () => {
   // const onFinish = (values) => {};
 
   return (
-<<<<<<< HEAD
-    <>
-      <div className={styles.container}>
-        <h2>Bussiness Details</h2>
-        <Collapse accordion style={{ width: "30vw" }}>
-          <Panel header="Your Details" key={1} style={{ fontWeight: "bold" }}>
-            <p>Your First Name</p>
-            <p>Your Last Name</p>
-          </Panel>
-          <Panel
-            header="Your Bussiness Details"
-            key={2}
-            style={{ fontWeight: "bold" }}
-          ></Panel>
-          <Panel header="Your Locations" key={3} style={{ fontWeight: "bold" }}>
-            <p>- Barcelona</p>
-            <p>- London</p>
-            <p>- Berlin</p>
-          </Panel>
-          <Panel header="Update your details" key={4}>
-            <Input
-              type="text"
-              placeholder="First Name"
-              className={styles.businessInput}
-            />
-            <Input
-              type="text"
-              placeholder="Last Name"
-              className={styles.businessInput}
-            />
-            <h3>Business Address</h3>
-            <Input
-              type="text"
-              placeholder="Number and Street name"
-              className={styles.businessInput}
-            />
-            <Input
-              type="text"
-              placeholder="Post code"
-              className={styles.businessInput}
-            />
-            <Input
-              type="text"
-              placeholder="Region"
-              className={styles.businessInput}
-            />
-            <Input
-              type="text"
-              placeholder="City"
-              className={styles.businessInput}
-            />
-            <Input
-              type="text"
-              onChange={handleChange}
-              placeholder="Type: (Onsite / Online)"
-              className={styles.businessInput}
-            />
-            <Button type='primary'>Submit</Button>
-          </Panel>
-        </Collapse>
-      </div>
-    </>
-=======
     <div className={styles.container}>
       <h2>Bussiness Details</h2>
       <Collapse accordion style={{ width: "30vw" }}>
@@ -225,7 +152,6 @@ const ProfileInfoTab = () => {
         </Panel>
       </Collapse>
     </div>
->>>>>>> refactor-new
   );
 };
 
